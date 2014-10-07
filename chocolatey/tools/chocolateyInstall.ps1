259111da70b22135f9ad2a0460962fc082b8670c
Tomas Jansson <tomas.jansson@gmail.com>
2014-10-07 08:22:55 +0200

    java-server-jre installs to program files

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
 $packageName = 'java-server-jre' 
 $url = 'https://github.com/mastoj/java-server-jre/raw/master/releases/jdk1.7.0_67.zip'
-$installFolder = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$programFiles = $env:ProgramFiles
+$installFolder = "$programFiles\java"
+
 Install-ChocolateyZipPackage "$packageName" "$url" "$installFolder"
 $java_home = "$installFolder\jdk1.7.0_67"
 [Environment]::SetEnvironmentVariable("JAVA_HOME", $java_home, "Machine")
